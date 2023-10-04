661255cc0c985ccf611dd92ec15b0ee8a40454fe
KrishJadhav <krishnamjadhav@outlokk.com>
2023-10-04 17:14:43 +0530

    added all files

---
+++ JavaScript/Objects/Object.js
@@ -0,0 +1,10 @@
+const person=new Object()
+    person.firstName=krishna;
+    person.lastName=Jadhav;
+   person.Village=udgir;
+
+
+
+console.log("Here are your data",person.firstname+"");
+
+//D:\projects\InterviewQuestions\JavaScript\Objects\Object.js
