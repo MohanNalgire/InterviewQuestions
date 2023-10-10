c6c8ba9e1e7daa99fcf72498031b9fbf58c4c8ff
mohanbalgire <mohannalgirework@gmail.com>
2023-10-10 10:34:24 +0530

    module examples

--- NodeJS/codeExamples/dataObj.js
+++ NodeJS/codeExamples/dataObj.js
@@ -1,36 +1,8 @@
- const data = [
-    {
+ const data = {
       testingNewValue: {
         "en-US": "hello",
       },
-    },
-  
-    {
-      testingNewValue: {
-        "hi-IN": "hello ",
-      },
-    },
-  
-    {
-      testingNewValue: {
-        us: "dummy",
-      },
-    },
-  
-    {
-      testingNewValue: {
-        "xp-op-as": "value for testing",
-      },
-  
-      locationField: {
-        "en-US": {
-          lat: 19.28563,
-  
-          lon: 72.8691,
-        },
-      },
-    },
-  ];
+    };
   
 // module => commonjs module
 module.exports = { data };
--- NodeJS/codeExamples/objectOperations.js
+++ NodeJS/codeExamples/objectOperations.js
@@ -1,8 +1,3 @@
 const data = require('./dataObj');
+
 console.log(data.data);
-/*
-const callback = function (obj, index) {
-  console.log(`index ${index}`, Object.keys(obj.testingNewValue)[0]);
-};
-data.data.forEach(callback);
-*/
