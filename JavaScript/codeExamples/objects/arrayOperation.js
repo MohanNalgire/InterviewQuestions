05da3345ab387560952e29c883120243c765f825
mohanbalgire <mohannalgirework@gmail.com>
2023-10-10 10:22:34 +0530

    array operations and file separation

---
+++ JavaScript/codeExamples/objects/arrayOperation.js
@@ -0,0 +1,8 @@
+// forEach example
+const callback = function (obj, index, /* array */) {
+    console.log(`index ${index}`, Object.keys(obj.testingNewValue)[0]);
+};
+data.forEach(callback);
+
+// reducer example
+
