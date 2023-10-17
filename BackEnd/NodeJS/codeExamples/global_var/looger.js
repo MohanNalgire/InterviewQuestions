10a19c6d42dd904a734dc319b0661428ea7ac59a
mohanbalgire <mohannalgirework@gmail.com>
2023-10-17 10:23:06 +0530

    new topics files'

---
+++ BackEnd/NodeJS/codeExamples/global_var/looger.js
@@ -0,0 +1,6 @@
+module.export = new logger(cstomLoger); // we are exporting as a module
+/*
+    If we want to export as a global variable then we will use following syntax
+*/
+// 
+global.logger = new logger();
