1bef930ef2e68f9407d0ad9413a02c7c24154109
MohanNalgire <mohannalgirework@gmail.com>
2024-09-15 15:18:10 +0530

    logical operators

--- FrontEnd/JavaScript/JavaScript_Basic/codeExamples/class/TestClass.js
+++ FrontEnd/JavaScript/JavaScript_Basic/codeExamples/class/TestClass.js
@@ -1,9 +1,9 @@
-class Test {
+class TestClass {
     constructor(last_name, first_name){
         this.last_name = last_name;
         this.first_name = first_name;
     }
 }
 
-const testObject = new Test('Nalgire', "Mohan");
+const testObject = new TestClass('Nalgire', "Mohan");
 console.log('Log testObject', testObject);
---
+++ FrontEnd/JavaScript/JavaScript_Basic/codeExamples/operators/logicalOperator/logical_or_assignment.js
@@ -0,0 +1,10 @@
+// What is output
+// Why output is 
+let a =5;
+a ||= 10;
+console.log(a);
+
+//2. 
+const b =5;
+b ||= 30;
+console.log(b);
