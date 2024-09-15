64874b0d5516987d22dc7dd63aa3c7f52dbd8d4f
MohanNalgire <mohannalgirework@gmail.com>
2024-09-15 13:17:13 +0530

    class basic example

---
+++ FrontEnd/JavaScript/JavaScript_Basic/codeExamples/class/class.js
@@ -0,0 +1,9 @@
+class Test {
+    constructor(last_name, first_name){
+        this.last_name = last_name;
+        this.first_name = first_name;
+    }
+}
+
+const testObject = new Test('Nalgire', "Mohan");
+console.log('Log testObject', testObject);
